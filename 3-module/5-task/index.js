b626a16cb2ceef210f85f418a8a015c53a896cdc
iPetlicki <83550863+iPetlicki@users.noreply.github.com>
2022-03-31 02:08:24 +0200

    решены задачи из Занятий 3 и 4

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,8 @@
 function getMinMax(str) {
-  // ваш код...
+ let arr = str.split(' ').filter(item => isFinite(item)).map(item => Number(item));
+ let result =  {
+      min : Math.min.apply(null, arr),
+      max : Math.max.apply(null, arr)
+    };
+    return result
 }
--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let list = document.createElement('ul');
+  document.body.append(list);
+  
+  for(let i = 0; i < friends.length; i++) {
+    li = document.createElement('li');
+    li.innerHTML = `${friends[i].firstName} ${friends[i].lastName}`;
+    list.append(li);
+  }
+  return list
 }
